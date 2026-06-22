acc570a8df888ee3d82926716def050abcccb7ba
paramveer singh <pvsb19@gmail.com>
2026-06-22 19:29:47 +0530

    Update asset.js

--- controller/asset.js
+++ controller/asset.js
@@ -53,3 +53,53 @@ exports.getAsset = (req,res,next) =>{
     })
     .catch(err => console.log(err))
 }
+
+
+exports.delete = (req,res,next) =>{
+    const id = req.params.id;
+    console.log('id',id)
+    Asset.deleteOne({_id: id}).then(result=>{
+        console.log(result)
+        console.log('deleted successfully')
+        return res.status(200).json({"message":'Article deleted successfully'})
+    }).catch(err => {
+        console.log('inside catch');
+        const error = new Error('No results found for this post');
+
+          next(error)
+    
+        }
+    )
+}
+
+exports.updatePost = (req,res,next) =>{
+    const id = req.params.id;
+    const link = req?.body?.link;
+    const category = req?.body?.category;
+    const title = req?.body?.title;
+    const subCategory = req?.body?.subCategory;
+    const frameWork = req?.body?.frameWork;
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        const error = new Error('Invalid data');
+        error.statusCode = 500;
+        console.log(errors)
+        return res.status(500).json({message:'Invalid data',errors: errors})  
+
+        // throw error;
+    }
+
+    Asset.updateOne(
+        {_id: id},
+        {$set:{title:title,link:link,category: category,subCategory: subCategory,framework: frameWork}}
+
+    )
+    .then((result)=>{
+        console.log(result)
+        return res.status(201).json({message:"Post updated successfully!!"})
+    })
+    .catch(err =>{
+        next(err)
+    })
+}
