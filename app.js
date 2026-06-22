abcf96498f0e1e2a27a41eaa267c78131bc1dd18
paramveer singh <pvsb19@gmail.com>
2026-06-22 19:17:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const asset  = require('./routes/asset');
 dotenv.config(); 
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers','Content-Type');
     next()
 })
